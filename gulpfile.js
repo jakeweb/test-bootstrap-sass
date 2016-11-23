ab7b209be2a59f725684683fb264edf2f81e69d8
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-11-24 00:42:45 +0200

    add fonts

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,16 @@ var sass = require('gulp-sass');
 
 var path = {
     build: { //building files
-        css: 'website/assets/css/'
+        css: 'website/build/css/'
     },
     watch: { //watch changes form those files
-        sass: 'website/assets/sass/**/*.sass'
-    },
-    clean: 'frontend/build'
+        sass: 'website/sass/*.scss'
+    }
 };
 gulp.task('sass', function() {
     return gulp.src(path.watch.sass)
         .pipe(sass())
-        .pipe(gulp.dest('website/assets/css/'));
+        .pipe(gulp.dest('website/build/css/'));
 });
 
 // Rerun the task when a file changes
