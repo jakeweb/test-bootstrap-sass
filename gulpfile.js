ce742bb3fbe92b310d2b8734e0f1f813ec9839c3
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-11-23 23:45:07 +0200

    create folder structure

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+
+var path = {
+    build: { //building files
+        css: 'website/assets/css/'
+    },
+    watch: { //watch changes form those files
+        sass: 'website/assets/sass/**/*.sass'
+    },
+    clean: 'frontend/build'
+};
+gulp.task('sass', function() {
+    return gulp.src(path.watch.sass)
+        .pipe(sass())
+        .pipe(gulp.dest('website/assets/css/'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(path.watch.sass, ['sass']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['sass']);
